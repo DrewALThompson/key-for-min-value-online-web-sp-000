f0a9647a9ce7124641d952f6ad4f33da6dd06770
Drew Thompson <drewallynthompson@gmail.com>
2020-08-31 07:48:05 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min = []
+  min = 0
+  lowest_key = nil
  name_hash.each do |name, value|
-   if value >
+   if value > min || value == 0
 end
